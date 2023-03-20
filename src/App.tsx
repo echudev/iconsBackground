70b498d1a29bb592f31d3801a1c944e2cff2c066
Ezequiel <ezequiel.maranda@gmail.com>
2023-03-20 20:38:16 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+function App () {
+  return (
+    <div>
+      <header>
+        <h3>Hola, soy un nuevo portfolio despues de un mes sin tocar nada</h3>
+      </header>
+    </div>
+  )
+}
+
+export default App
