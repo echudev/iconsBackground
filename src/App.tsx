132026a7aac2822e4841461105086f65df123cf4
Ezequiel <ezequiel.maranda@gmail.com>
2023-04-14 00:59:42 -0300

    useRef removed, add window object to get innerScreen sizes

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,19 @@
 import { Outlet } from 'react-router-dom'
 import Header from './components/Header'
-import { useRef, useEffect } from 'react'
+import { useEffect } from 'react'
 import { useMousePositionStore } from './store/mousePositionStore'
 
 function App() {
-  const container = useRef<HTMLDivElement | null>(null)
   const { setHalfWidth, setHalfHeight, handleMousePosition } =
     useMousePositionStore()
 
   useEffect(() => {
-    if (container.current) {
-      const { width, height } = container.current.getBoundingClientRect()
-      setHalfWidth(width / 2)
-      setHalfHeight(height / 2)
-    }
-  }, [window.innerWidth, window.innerHeight, container.current])
+    setHalfWidth(window.innerWidth / 2)
+    setHalfHeight(window.innerHeight / 2)
+  }, [window.innerWidth, window.innerHeight])
 
   return (
     <div // This is the div that contains the entire app
-      ref={container}
       onMouseMove={e => handleMousePosition(e)}>
       <Header />
       <Outlet />
